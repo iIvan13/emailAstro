485b0023c84d848333ec57c69e9d8d78c2871f4c
Iv44n <ivandev2oo6@gmail.com>
2024-01-13 18:48:59 -0500

    app resend email

---
+++ .netlify/functions-internal/ssr/chunks/index_YFwSg9oq.mjs
@@ -0,0 +1,6 @@
+export { renderers } from '../renderers.mjs';
+export { onRequest } from '../_empty-middleware.mjs';
+
+const page = () => import('./pages/index_sn4qWewN.mjs');
+
+export { page };
---
+++ src/pages/api/sendEmail.json.ts
@@ -0,0 +1,49 @@
+import { Resend } from "resend";
+
+const resend = new Resend(import.meta.env.RESEND_API_KEY);
+
+export const POST = async ({ params, request }) => {
+  const body = await request.json();
+
+  const { to, from, html, subject, text } = body;
+
+  if (!to || !from || !html || !subject || !text) {
+    return new Response(null, {
+      status: 404,
+      statusText: "Did not provide the right data",
+    });
+  }
+  const { data, error } = await resend.emails.send({
+    from,
+    to,
+    subject,
+    html,
+    text,
+  });
+
+  if (data) {
+    return new Response(
+      JSON.stringify({
+        message: data,
+      }),
+      {
+        status: 200,
+        statusText: "OK",
+      }
+    );
+  } else {
+    return new Response(
+      JSON.stringify({
+        message: error,
+      }),
+      {
+        status: 500,
+        statusText: "Internal Server Error",
+      }
+    );
+  }
+
+  if (error) {
+    return console.error({ error });
+  }
+};
