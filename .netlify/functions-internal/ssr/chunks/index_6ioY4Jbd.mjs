7ca6ff121956d93fb1730c37ac74675f917caf7b
Iv44n <ivandev2oo6@gmail.com>
2024-01-13 20:21:01 -0500

    error add

--- .netlify/functions-internal/ssr/chunks/index_6ioY4Jbd.mjs
+++ .netlify/functions-internal/ssr/chunks/index_6ioY4Jbd.mjs
@@ -1,6 +1,6 @@
 export { renderers } from '../renderers.mjs';
 export { onRequest } from '../_empty-middleware.mjs';
 
-const page = () => import('./pages/index_jXnUdUTa.mjs');
+const page = () => import('./pages/index_iUIcROTu.mjs');
 
 export { page };
